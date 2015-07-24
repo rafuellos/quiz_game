5546626d55ecd3e6123ae4ef60a336aed6c52cd6
Rafael Coomonte <rafuellos@gmail.com>
2015-07-24 15:28:43 +0200

    Points feature included

--- quiz.js
+++ quiz.js
@@ -5,26 +5,29 @@ var Question = function (question, answer, questionId) {
 	this.question = question;
 	this.answer = answer;
 	this.questionId = questionId;
+	this.points = 5;
 
 }
 
 var Quiz = function () {
 	this.questionList = [];
 	this.questionNumber = 0;
+	this.totalPoints = 0;
+	this.start = true;
 
 }
 
 Quiz.prototype.gameOn = function() {
-	if (this.questionNumber === 0){
+	if (this.start === true){
 		this.shuffleQuestions(this.questionList);
 	}
-	this.questionList
+	//console.log(this.questionNumber);
 	
 	if (this.questionNumber < this.questionList.length) {
 		this.showQuestion(this.questionNumber);
 	} else
 	{
-		console.log ('\n\n\Fin del Juego');
+		console.log ('\n\n\End of the Game\n\n Yout earned a total of ' + this.totalPoints + ' points!');
 	}
 };
 
@@ -43,8 +46,8 @@ Quiz.prototype.shuffleQuestions = function(questionList) {
 
 Quiz.prototype.showQuestion = function(number) {
 
-		response = {prompt: '\n\n' + this.questionList[number].question + '?\n>', default: 'One single word'};
-		read(response, this.checkAnswer.bind(this));
+	response = {prompt: '\n\n' + this.questionList[number].question + '?\n>', default: 'One single word'};
+	read(response, this.checkAnswer.bind(this));
 	
 };
 
@@ -65,13 +68,16 @@ Quiz.prototype.checkAnswer = function (err, answer) {
 		//			 questionAnswer);
 		
 		if (answer.toLowerCase() === questionAnswer.toLowerCase()) {
-			console.log(this.questionNumber);
+			this.totalPoints += this.questionList[this.questionNumber].points;
+			console.log('Points: ' + this.totalPoints);
 			this.questionNumber++;
 			console.log ('CORRECT!!!\n');
+			this.start = false;
 			this.gameOn();
 		}
 		else {
 			this.failAnswer();
+			this.start = false;
 			this.gameOn();
 		}
 
@@ -81,22 +87,27 @@ Quiz.prototype.failAnswer = function() {
 	var failMessagges = ['No, no!\n', 'Give it another try\n', 
 						'Not your better day\n', 'Do you want to surrender?\n'];
 	var failMessagge = failMessagges[Math.floor(Math.random()*failMessagges.length)];
+	this.questionList[this.questionNumber].points--;
 	console.log (failMessagge);
 };
 
 var question1 = new Question ('The country of Italy includes two major islands. Which is the largest one?',
-							'Sicily',
-							1
-							);
-var question2 = new Question ('How may legs has a common dog?', '4',
-							1
-							);
+							'Sicily', 1);
+var question2 = new Question ('How may legs has a common dog?', '4', 2);
+var question3 = new Question ('What nationality was Chopin?', 'Polish', 3);
+var question4 = new Question ('What year did Elvis Presley die?', '1977', 4);
+var question5 = new Question ('Which is the largest ocean?', 'Pacific', 5);
+var question6 = new Question ('What money do they use in Japan?', 'Yen', 6);
+var question7 = new Question ('How many months have 28 days?', '12',	7);
+var question8 = new Question ('Which is the only mammal that is not capable of jumping?',
+							 'Elephant', 8);
+var question9 = new Question ('Who said E=mc2?', 'Einstein', 9);
+var question10 = new Question ('Who said, "Vini, vidi, vici"?', 'Caesar',	10);
+
 
 var quiz = new Quiz();
-//quiz.resetStack();
-quiz.addQuestion([question1,question2]);
-//quiz.addQuestion(question2);
-//console.log(quiz.questionList);
+quiz.addQuestion([question1 ,question2, question3, question4, question5,
+				 question6, question7, question8, question9, question10]);
 
 quiz.gameOn();
 
