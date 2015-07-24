109d04b76fc2f41a3a4b37dd28a9c06e4ee97ac5
Rafael Coomonte <rafuellos@gmail.com>
2015-07-24 14:56:39 +0200

    Included a shuffle questions function

--- quiz.js
+++ quiz.js
@@ -14,16 +14,38 @@ var Quiz = function () {
 
 }
 
-
-Quiz.prototype.showQuestion = function() {
+Quiz.prototype.gameOn = function() {
+	if (this.questionNumber === 0){
+		this.shuffleQuestions(this.questionList);
+	}
+	this.questionList
 	
 	if (this.questionNumber < this.questionList.length) {
-		response = {prompt: '\n\n' + this.questionList[this.questionNumber].question + '?\n>', default: 'One single word'};
-		read(response, this.checkAnswer.bind(this));
+		this.showQuestion(this.questionNumber);
 	} else
 	{
 		console.log ('\n\n\Fin del Juego');
 	}
+};
+
+Quiz.prototype.shuffleQuestions = function(questionList) {
+
+	for (var i = questionList.length-1; i >=0; i--) {
+	     
+	        var randomIndex = Math.floor(Math.random()*(i+1)); 
+	        var itemAtIndex = questionList[randomIndex]; 
+	         
+	        questionList[randomIndex] = questionList[i]; 
+	        questionList[i] = itemAtIndex;
+	    }
+	return questionList;
+};
+
+Quiz.prototype.showQuestion = function(number) {
+
+		response = {prompt: '\n\n' + this.questionList[number].question + '?\n>', default: 'One single word'};
+		read(response, this.checkAnswer.bind(this));
+	
 };
 
 
@@ -43,19 +65,21 @@ Quiz.prototype.checkAnswer = function (err, answer) {
 		//			 questionAnswer);
 		
 		if (answer.toLowerCase() === questionAnswer.toLowerCase()) {
+			console.log(this.questionNumber);
 			this.questionNumber++;
 			console.log ('CORRECT!!!\n');
-			this.showQuestion(this.questionNumber);
+			this.gameOn();
 		}
 		else {
 			this.failAnswer();
-			this.showQuestion(this.questionNumber);
+			this.gameOn();
 		}
 
 }
 
 Quiz.prototype.failAnswer = function() {
-	var failMessagges = ['No, no!\n', 'Give it another try\n', 'Not your better day\n', 'Do you want to surrender\n'];
+	var failMessagges = ['No, no!\n', 'Give it another try\n', 
+						'Not your better day\n', 'Do you want to surrender?\n'];
 	var failMessagge = failMessagges[Math.floor(Math.random()*failMessagges.length)];
 	console.log (failMessagge);
 };
@@ -74,5 +98,5 @@ quiz.addQuestion([question1,question2]);
 //quiz.addQuestion(question2);
 //console.log(quiz.questionList);
 
-quiz.showQuestion(1);
+quiz.gameOn();
 
